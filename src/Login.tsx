59f6d804d20a21cdd2801b17337b98dc1ff18834
Rafael Lima Barreto <rafaellbecom@gmail.com>
2024-05-09 23:42:40 -0300

    Instalando Expo e TypeScript, Configurando Native Base e Criando a Tela de Login

---
+++ src/Login.tsx
@@ -0,0 +1,85 @@
+import { VStack, Image, Text, Box, FormControl, Input, Button, Link } from 'native-base'
+import { TouchableOpacity } from 'react-native';
+import Logo from './assets/Logo.png'
+
+export default function Login() {
+  return (
+    <VStack flex={1} justifyContent="center" alignItems="center" p={5}>
+      <Image source={require('./assets/Logo.png')} alt="Logo Voll" />
+      
+      <Text
+        fontSize="2xl"
+        fontWeight="bold"
+        color="gray.500"
+        textAlign="center"
+        mt={5}
+      >
+        Faça login em sua conta
+      </Text>
+
+      <Box>
+        <FormControl mt={3}>
+          <FormControl.Label>Email</FormControl.Label>
+          <Input 
+            placeholder="Insira seu endereço de email"
+            size="lg"
+            w="100%"
+            borderRadius='lg'
+            bgColor='gray.100'
+            shadow={3}  
+          />
+        </FormControl>
+
+        <FormControl mt={3}>
+          <FormControl.Label>Senha</FormControl.Label>
+          <Input 
+            placeholder="Insira sua senha"
+            size="lg"
+            w="100%"
+            borderRadius="lg"
+            bgColor="gray.100"
+            shadow={3}  
+          />
+        </FormControl>
+      </Box>
+
+      <Button
+        w="100%"
+        bgColor="blue.800"
+        mt={10}
+        borderRadius="lg"
+      >
+        Entrar
+      </Button>
+
+      <Link
+        href="https://alura.com.br"
+        mt={2}
+      >
+        Esqueceu sua senha?
+      </Link>
+
+      <Box
+        w="100%"
+        mt={12}
+        flexDirection="row"
+        justifyContent="center"
+      >
+        <Text
+          color="blue.800"
+        >
+          Ainda não tem cadastro? 
+        </Text>
+        <TouchableOpacity>
+          <Text
+            color="blue.500"
+            fontWeight="bold"
+          >
+            Faça seu cadastro
+          </Text>
+        </TouchableOpacity>
+      </Box>
+    </VStack>
+  );
+}
+
